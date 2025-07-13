4b3de1e40236d6e75597b197fcd1e0c9c1f0100b
Saadnaiem <s_pharma2008@yahoo.com>
2025-07-13 03:12:24 +0300

    Fix: Rename Index.html to index.html for GitHub Pages compatibility

---
+++ start-local-server.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting Al Habib Baby+ Club Website...
+echo.
+echo Your website will be available at: http://localhost:8000
+echo Open your browser and go to: http://localhost:8000/index.html
+echo Press Ctrl+C to stop the server
+echo.
+python -m http.server 8000
+pause
